f65ca8b8335cf791431949cc3bcd9ad73c91aab0
Nguyen Hai-Son <nhson17@apcs.vn>
2020-02-06 23:31:09 +0700

    Fun changes

--- InitialSetUp/index.js
+++ InitialSetUp/index.js
@@ -3,9 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Báo cáo sếp. Đã nhận được lệnh!");
+  res.send("<h1>Báo cáo sếp. Đã nhận được lệnh!</h1>");
+});
+
+app.get("/captain", (req, res) => {
+  res.send("<h1>Cái gì, có kẻ giả danh ta sao...</h1>");
 });
 
 app.listen(port, () => {
-  console.log("Báo cáo sếp. Server đang phát!");
+  console.log(`Báo cáo sếp. Server đang được phát trên ${port}!`);
 });
