b8820139301e01b7b0f80155afba6bb740a589e4
Nguyen Hai-Son <nhson17@apcs.vn>
2020-02-07 15:28:39 +0700

    NodejsExpress-Bài 2

--- InitialSetUp/index.js
+++ InitialSetUp/index.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.get("/", (req, res) => {
-  res.send("<h1>Báo cáo sếp. Đã nhận được lệnh!</h1>");
+  res.render("index.pug", {
+    name: "H.Sơn"
+  });
+});
+
+app.get("/users", (req, res) => {
+  res.render("users/index.pug", {
+    users: [
+      { id: 1, name: "Yasuo" },
+      { id: 2, name: "Luffy" }
+    ]
+  });
 });
 
 app.get("/captain", (req, res) => {
